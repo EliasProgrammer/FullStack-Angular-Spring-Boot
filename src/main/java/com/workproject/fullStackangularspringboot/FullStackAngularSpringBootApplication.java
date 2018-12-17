849c595b42f75f74c8eacd2ab743c4262d35fcb5
EliasVMendes <elias.mendes.2009@gmail.com>
2018-12-17 21:50:30 -0200

    Criando o projeto da API

---
+++ src/main/java/com/workproject/fullStackangularspringboot/FullStackAngularSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.workproject.fullStackangularspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackAngularSpringBootApplication.class, args);
+	}
+
+}
+
