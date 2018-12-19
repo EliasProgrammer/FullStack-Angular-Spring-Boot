bcddd249e03435c35a9c7b4999318b07516c52b7
Elias Mendes <elias.mendes.2009@gmail.com>
2018-12-19 11:27:02 -0200

    Merge da CategoriaResource

--- src/main/java/com/workproject/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/workproject/algamoney/api/resource/CategoriaResource.java
@@ -1,25 +1,45 @@
 package com.workproject.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.workproject.algamoney.api.model.Categoria;
-import com.workproject.algamoney.api.repository.CategoriaRepository;
+import com.workproject.algamoney.api.service.CategoriaService;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
 	@Autowired
-	private CategoriaRepository categoriaRepository;
+	private CategoriaService categoriaSevice;
 	
 	@GetMapping
 	public List<Categoria> listar(){
-		return categoriaRepository.findAll();
+		return categoriaSevice.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria){
+		Categoria categoriaSalva = categoriaSevice.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo){
+		return categoriaSevice.findById(codigo);
 	}
 	
 }
